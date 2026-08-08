5f14f683d9e18a8dfc4f1353a7acfc5782756001
Jad K. Haddad <jadkhaddad@gmail.com>
2026-08-08 09:55:50 +0200

    refactor(rusmpp-core/parts): rework how we parse lifetimes for serde bound
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmpp-macros/src/parts.rs
+++ rusmpp-macros/src/parts.rs
@@ -37,16 +37,22 @@ pub fn quote_parts(input: &DeriveInput, fields_named: &FieldsNamed) -> TokenStre
         .clone()
         .map(|ident| quote! { #ident: self.#ident });
 
-    let serde_bound = if input.generics.lifetimes().next().is_some() {
-        quote! {
-            #[cfg_attr(
-                feature = "serde",
-                serde(bound(deserialize = "'de: 'a"))
-            )]
-        }
-    } else {
-        quote! {}
-    };
+    let serde_bound = input
+        .generics
+        .lifetimes()
+        .next()
+        .map(|param| {
+            let lifetime = &param.lifetime;
+            let bound = syn::LitStr::new(&format!("'de: {}", lifetime), lifetime.span());
+
+            quote! {
+                #[cfg_attr(
+                    feature = "serde",
+                    serde(bound(deserialize = #bound))
+                )]
+            }
+        })
+        .unwrap_or_default();
 
     quote! {
         #[derive(Debug)]
