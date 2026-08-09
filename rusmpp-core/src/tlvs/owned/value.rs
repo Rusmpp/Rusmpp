05473dbb8ead62830bea5eb74d61197312a02f2f
Jad K. Haddad <jadkhaddad@gmail.com>
2026-08-09 18:58:49 +0200

    fix(rusmpp-core/re-export/values): fixed how errors are exported only under the alloc feature
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmpp-core/src/tlvs/owned/value.rs
+++ rusmpp-core/src/tlvs/owned/value.rs
@@ -11,7 +11,6 @@ use crate::{
     tlvs::TlvTag,
     types::owned::{AnyOctetString, COctetString, OctetString},
     values::{
-        errors::*,
         owned::{errors::*, *},
         *,
     },
