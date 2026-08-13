6f7747112246c05f3560b40bc8090c92ef02b56e
Jad K. Haddad <jadkhaddad@gmail.com>
2026-08-13 09:49:39 +0200

    feat(rusmpp-core/values): added repr/serde transparent to broadcast rep num
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmpp-core/src/values/broadcast_rep_num.rs
+++ rusmpp-core/src/values/broadcast_rep_num.rs
@@ -1,9 +1,11 @@
 use rusmpp_macros::Rusmpp;
 
 /// This field indicates the number of repeated broadcasts requested by the Submitter.
+#[repr(transparent)]
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Rusmpp)]
 #[cfg_attr(feature = "arbitrary", derive(::arbitrary::Arbitrary))]
 #[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(transparent))]
 pub struct BroadcastRepNum {
     pub value: u8,
 }
