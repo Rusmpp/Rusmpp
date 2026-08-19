72014c0e8c06e9ae750e61e3106eced0a9b8f53d
Jad K. Haddad <jadkhaddad@gmail.com>
2026-08-19 21:42:48 +0200

    dev(rusmpp-core/serde/borrowed): experimenting with serde `a [u8]
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmpp-core/src/command/borrowed.rs
+++ rusmpp-core/src/command/borrowed.rs
@@ -2,8 +2,6 @@ use rusmpp_macros::Rusmpp;
 
 use crate::{CommandId, CommandStatus, pdus::borrowed::Pdu};
 
-// TODO: serde like owned.
-
 /// `SMPP` command.
 ///
 /// The following PDU example illustrates how a `SMPP` PDU is decoded:
@@ -40,8 +38,6 @@ use crate::{CommandId, CommandStatus, pdus::borrowed::Pdu};
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Rusmpp)]
 #[rusmpp(decode = borrowed)]
 #[cfg_attr(feature = "arbitrary", derive(::arbitrary::Arbitrary))]
-#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
-#[cfg_attr(feature = "serde", serde(bound(deserialize = "'de: 'a")))]
 pub struct Command<'a, const N: usize> {
     /// See [`CommandId`]
     id: CommandId,
@@ -161,6 +157,67 @@ impl<'a, const N: usize> PduBuilder<'a, N> {
     }
 }
 
+#[cfg(feature = "serde")]
+const _: () = {
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    use crate::types::borrowed::AnyOctetString;
+
+    #[derive(Serialize)]
+    struct SerCommand<'a, const N: usize> {
+        status: CommandStatus,
+        sequence_number: u32,
+        pdu: &'a Pdu<'a, N>,
+    }
+
+    impl<'a, const N: usize> Serialize for Command<'a, N> {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let pdu = Pdu::Other {
+                command_id: self.id(),
+                body: AnyOctetString::empty(),
+            };
+
+            let pdu = self.pdu.as_ref().unwrap_or(&pdu);
+
+            let command = SerCommand {
+                status: self.status,
+                sequence_number: self.sequence_number,
+                pdu,
+            };
+
+            command.serialize(serializer)
+        }
+    }
+
+    #[derive(Deserialize)]
+    #[serde(bound(deserialize = "'de: 'a"))]
+    struct DeCommand<'a, const N: usize> {
+        status: CommandStatus,
+        sequence_number: u32,
+        pdu: Pdu<'a, N>,
+    }
+
+    impl<'a, const N: usize> From<DeCommand<'a, N>> for Command<'a, N> {
+        fn from(command: DeCommand<'a, N>) -> Self {
+            Self::new(command.status, command.sequence_number, command.pdu)
+        }
+    }
+
+    impl<'de: 'a, 'a, const N: usize> Deserialize<'de> for Command<'a, N> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let command = DeCommand::deserialize(deserializer)?;
+
+            Ok(Self::from(command))
+        }
+    }
+};
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -168,5 +225,103 @@ mod tests {
     #[test]
     fn encode_decode() {
         crate::tests::borrowed::encode_decode_with_length_test_instances::<Command<'static, 16>>();
+    }
+
+    #[cfg(feature = "serde")]
+    mod serde {
+        use std::println;
+
+        use crate::{
+            pdus::borrowed::SubmitSm,
+            values::{GenericServiceType, borrowed::ServiceType},
+        };
+
+        use super::*;
+
+        #[test]
+        // #[ignore = "Prints the serialized JSON for manual inspection"]
+        fn serialize() {
+            let command = Command::<'_, 16>::builder()
+                .status(CommandStatus::EsmeRok)
+                .sequence_number(1)
+                .pdu(
+                    SubmitSm::builder()
+                        .service_type(ServiceType::new(
+                            GenericServiceType::CellularMessaging.into(),
+                        ))
+                        .build(),
+                );
+
+            let mut serialized = [0u8; 1024];
+            let len = serde_json_core::to_slice(&command, &mut serialized[..])
+                .expect("Failed to serialize command");
+
+            println!("{:?}", &serialized[..len]);
+        }
+
+        #[test]
+        fn deserialize() {
+            let json: &[u8] = &[
+                123, 34, 115, 116, 97, 116, 117, 115, 34, 58, 34, 69, 115, 109, 101, 82, 111, 107,
+                34, 44, 34, 115, 101, 113, 117, 101, 110, 99, 101, 95, 110, 117, 109, 98, 101, 114,
+                34, 58, 49, 44, 34, 112, 100, 117, 34, 58, 123, 34, 83, 117, 98, 109, 105, 116, 83,
+                109, 34, 58, 123, 34, 115, 101, 114, 118, 105, 99, 101, 95, 116, 121, 112, 101, 34,
+                58, 67, 77, 84, 0, 44, 34, 115, 111, 117, 114, 99, 101, 95, 97, 100, 100, 114, 95,
+                116, 111, 110, 34, 58, 34, 85, 110, 107, 110, 111, 119, 110, 34, 44, 34, 115, 111,
+                117, 114, 99, 101, 95, 97, 100, 100, 114, 95, 110, 112, 105, 34, 58, 34, 85, 110,
+                107, 110, 111, 119, 110, 34, 44, 34, 115, 111, 117, 114, 99, 101, 95, 97, 100, 100,
+                114, 34, 58, 0, 44, 34, 100, 101, 115, 116, 95, 97, 100, 100, 114, 95, 116, 111,
+                110, 34, 58, 34, 85, 110, 107, 110, 111, 119, 110, 34, 44, 34, 100, 101, 115, 116,
+                95, 97, 100, 100, 114, 95, 110, 112, 105, 34, 58, 34, 85, 110, 107, 110, 111, 119,
+                110, 34, 44, 34, 100, 101, 115, 116, 105, 110, 97, 116, 105, 111, 110, 95, 97, 100,
+                100, 114, 34, 58, 0, 44, 34, 101, 115, 109, 95, 99, 108, 97, 115, 115, 34, 58, 123,
+                34, 109, 101, 115, 115, 97, 103, 105, 110, 103, 95, 109, 111, 100, 101, 34, 58, 34,
+                68, 101, 102, 97, 117, 108, 116, 34, 44, 34, 109, 101, 115, 115, 97, 103, 101, 95,
+                116, 121, 112, 101, 34, 58, 34, 68, 101, 102, 97, 117, 108, 116, 34, 44, 34, 97,
+                110, 115, 105, 52, 49, 95, 115, 112, 101, 99, 105, 102, 105, 99, 34, 58, 34, 78,
+                111, 116, 83, 101, 108, 101, 99, 116, 101, 100, 34, 44, 34, 103, 115, 109, 95, 102,
+                101, 97, 116, 117, 114, 101, 115, 34, 58, 34, 78, 111, 116, 83, 101, 108, 101, 99,
+                116, 101, 100, 34, 125, 44, 34, 112, 114, 111, 116, 111, 99, 111, 108, 95, 105,
+                100, 34, 58, 48, 44, 34, 112, 114, 105, 111, 114, 105, 116, 121, 95, 102, 108, 97,
+                103, 34, 58, 48, 44, 34, 115, 99, 104, 101, 100, 117, 108, 101, 95, 100, 101, 108,
+                105, 118, 101, 114, 121, 95, 116, 105, 109, 101, 34, 58, 0, 44, 34, 118, 97, 108,
+                105, 100, 105, 116, 121, 95, 112, 101, 114, 105, 111, 100, 34, 58, 0, 44, 34, 114,
+                101, 103, 105, 115, 116, 101, 114, 101, 100, 95, 100, 101, 108, 105, 118, 101, 114,
+                121, 34, 58, 123, 34, 109, 99, 95, 100, 101, 108, 105, 118, 101, 114, 121, 95, 114,
+                101, 99, 101, 105, 112, 116, 34, 58, 34, 78, 111, 77, 99, 68, 101, 108, 105, 118,
+                101, 114, 121, 82, 101, 99, 101, 105, 112, 116, 82, 101, 113, 117, 101, 115, 116,
+                101, 100, 34, 44, 34, 115, 109, 101, 95, 111, 114, 105, 103, 105, 110, 97, 116,
+                101, 100, 95, 97, 99, 107, 110, 111, 119, 108, 101, 100, 103, 101, 109, 101, 110,
+                116, 34, 58, 34, 78, 111, 82, 101, 99, 101, 105, 112, 116, 83, 109, 101, 65, 99,
+                107, 110, 111, 119, 108, 101, 100, 103, 101, 109, 101, 110, 116, 82, 101, 113, 117,
+                101, 115, 116, 101, 100, 34, 44, 34, 105, 110, 116, 101, 114, 109, 101, 100, 105,
+                97, 116, 101, 95, 110, 111, 116, 105, 102, 105, 99, 97, 116, 105, 111, 110, 34, 58,
+                34, 78, 111, 73, 110, 116, 101, 114, 109, 101, 100, 105, 97, 114, 121, 78, 111,
+                116, 105, 102, 105, 99, 97, 116, 105, 111, 110, 82, 101, 113, 117, 101, 115, 116,
+                101, 100, 34, 44, 34, 111, 116, 104, 101, 114, 34, 58, 48, 125, 44, 34, 114, 101,
+                112, 108, 97, 99, 101, 95, 105, 102, 95, 112, 114, 101, 115, 101, 110, 116, 95,
+                102, 108, 97, 103, 34, 58, 34, 68, 111, 78, 111, 116, 82, 101, 112, 108, 97, 99,
+                101, 34, 44, 34, 100, 97, 116, 97, 95, 99, 111, 100, 105, 110, 103, 34, 58, 34, 77,
+                99, 83, 112, 101, 99, 105, 102, 105, 99, 34, 44, 34, 115, 109, 95, 100, 101, 102,
+                97, 117, 108, 116, 95, 109, 115, 103, 95, 105, 100, 34, 58, 48, 44, 34, 115, 109,
+                95, 108, 101, 110, 103, 116, 104, 34, 58, 48, 44, 34, 115, 104, 111, 114, 116, 95,
+                109, 101, 115, 115, 97, 103, 101, 34, 58, 44, 34, 116, 108, 118, 115, 34, 58, 91,
+                93, 125, 125, 125,
+            ];
+
+            let (command, _) = serde_json_core::from_slice::<Command<'_, 16>>(json)
+                .expect("Failed to deserialize command");
+
+            assert_eq!(command.status(), CommandStatus::EsmeRok);
+            assert_eq!(command.sequence_number(), 1);
+
+            let pdu = command.pdu().expect("Expected PDU to be present");
+            let pdu = match pdu {
+                Pdu::SubmitSm(submit_sm) => submit_sm,
+                _ => panic!("Expected PDU to be SubmitSm"),
+            };
+
+            assert_eq!(pdu, &SubmitSm::builder().build());
+        }
     }
 }
