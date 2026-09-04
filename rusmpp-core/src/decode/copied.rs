87edadc630facbcc0e214cd7d145abdf56fa9bed
Jad K. Haddad <jadkhaddad@gmail.com>
2026-09-04 11:42:15 +0200

    test(rusmpp-core/decode/copied): added tests for copied decode traits
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- rusmpp-core/src/decode/copied.rs
+++ rusmpp-core/src/decode/copied.rs
@@ -207,3 +207,217 @@ impl<T: Decode> DecodeWithLength for alloc::vec::Vec<T> {
         Ok((vec, size))
     }
 }
+
+#[cfg(feature = "alloc")]
+#[cfg(test)]
+mod tests {
+    use alloc::vec::Vec;
+
+    use crate::{
+        decode::{COctetStringDecodeError, IntegerDecodeError},
+        types::owned::{COctetString, EmptyOrFullCOctetString},
+    };
+
+    use super::*;
+
+    /// Testing [`counted_move`](DecodeExt::counted_move) will automatically test [`counted`](DecodeExt::counted).
+    #[test]
+    fn counted() {
+        // Count is 0
+        let buf = &[0, 1, 2];
+
+        let (values, size) = u8::counted(buf, 0).unwrap();
+
+        assert_eq!(size, 0);
+        assert_eq!(values.len(), 0);
+        assert_eq!(&buf[size..], &[0, 1, 2]);
+        assert_eq!(values, Vec::<u8>::new());
+
+        // Count is more than the buffer
+        let buf = &[0, 1, 2];
+
+        let error = u8::counted(buf, 5).unwrap_err();
+        assert!(matches!(error, IntegerDecodeError::UnexpectedEndOfBuffer));
+
+        // Count is within the buffer
+        let buf = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let (values, size) = u8::counted(buf, 10).unwrap();
+
+        assert_eq!(size, 10);
+        assert_eq!(values.len(), 10);
+        assert!(&buf[size..].is_empty());
+        assert_eq!(values, alloc::vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        let buf = &[0, 0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9];
+
+        let (values, size) = u16::counted(buf, 10).unwrap();
+
+        assert_eq!(size, 20);
+        assert_eq!(values.len(), 10);
+        assert!(&buf[size..].is_empty());
+        assert_eq!(values, alloc::vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        let buf = &[
+            0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0, 5, 0, 0, 0, 6, 0,
+            0, 0, 7, 0, 0, 0, 8, 0, 0, 0, 9,
+        ];
+
+        // Actually 10 values, 12 will break
+        let error = u32::counted(buf, 12).unwrap_err();
+
+        assert!(matches!(error, IntegerDecodeError::UnexpectedEndOfBuffer));
+
+        let buf = b"Hello\0World\0";
+
+        let (values, size) = COctetString::<1, 6>::counted_move(buf, 2, 0).unwrap();
+
+        assert_eq!(size, 12);
+        assert!(&buf[size..].is_empty());
+        assert_eq!(
+            values,
+            alloc::vec![
+                COctetString::<1, 6>::from_static_slice(b"Hello\0").unwrap(),
+                COctetString::<1, 6>::from_static_slice(b"World\0").unwrap(),
+            ]
+        );
+
+        let buf = b"Hello\0World\0";
+
+        let (values, size) = EmptyOrFullCOctetString::<6>::counted_move(buf, 2, 0).unwrap();
+
+        assert_eq!(size, 12);
+        assert!(&buf[size..].is_empty());
+        assert_eq!(
+            values,
+            alloc::vec![
+                EmptyOrFullCOctetString::<6>::from_static_slice(b"Hello\0").unwrap(),
+                EmptyOrFullCOctetString::<6>::from_static_slice(b"World\0").unwrap(),
+            ]
+        );
+
+        let buf = b"Hello\0World\0Hi";
+
+        let error = COctetString::<1, 6>::counted_move(buf, 3, 0).unwrap_err();
+
+        assert!(matches!(error, COctetStringDecodeError::NotNullTerminated));
+
+        // Remaining bytes
+        let buf = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let (values, size) = u8::counted_move(buf, 5, 0).unwrap();
+
+        assert_eq!(size, 5);
+        assert_eq!(&buf[size..], &[5, 6, 7, 8, 9]);
+        assert_eq!(values, alloc::vec![0, 1, 2, 3, 4]);
+
+        let buf = &[0, 0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9];
+
+        let (values, size) = u16::counted_move(buf, 5, 0).unwrap();
+
+        assert_eq!(size, 10);
+        assert_eq!(&buf[size..], &[0, 5, 0, 6, 0, 7, 0, 8, 0, 9]);
+        assert_eq!(values, alloc::vec![0, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn decode_with_length_vec() {
+        // Length is 0
+        let buf = &[0, 1, 2];
+
+        let (values, size) = Vec::<u8>::decode(buf, 0).unwrap();
+
+        assert_eq!(size, 0);
+        assert_eq!(&buf[size..], &[0, 1, 2]);
+        assert_eq!(values, Vec::<u8>::new());
+
+        // Length is bigger than the buffer
+        let buf = &[0, 1, 2];
+
+        let error = Vec::<u8>::decode(buf, 5).unwrap_err();
+
+        assert!(matches!(error, VecDecodeError::UnexpectedEndOfBuffer));
+
+        // Length is within the buffer
+        let buf = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let (values, size) = Vec::<u8>::decode(buf, 10).unwrap();
+
+        assert_eq!(size, 10);
+        assert!(&buf[size..].is_empty());
+        assert_eq!(values, alloc::vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        let buf = &[0, 0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9];
+
+        let (values, size) = Vec::<u16>::decode(buf, 20).unwrap();
+
+        assert_eq!(size, 20);
+        assert!(&buf[size..].is_empty());
+        assert_eq!(values, alloc::vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        let buf = &[
+            0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 0, 5, 0, 0, 0, 6, 0,
+            0, 0, 7, 0, 0, 0, 8, 0, 0, 0, 9,
+        ];
+
+        // Actually 40 bytes, 50 will break
+        let error = Vec::<u32>::decode(buf, 50).unwrap_err();
+
+        assert!(matches!(error, VecDecodeError::UnexpectedEndOfBuffer));
+
+        let buf = b"Hello\0World\0";
+
+        let (values, size) = Vec::<COctetString<1, 6>>::decode(buf, 12).unwrap();
+
+        assert_eq!(size, 12);
+        assert!(&buf[size..].is_empty());
+        assert_eq!(
+            values,
+            alloc::vec![
+                COctetString::<1, 6>::from_static_slice(b"Hello\0").unwrap(),
+                COctetString::<1, 6>::from_static_slice(b"World\0").unwrap(),
+            ]
+        );
+
+        let buf = b"Hello\0World\0";
+
+        let (values, size) = Vec::<EmptyOrFullCOctetString<6>>::decode(buf, 12).unwrap();
+
+        assert_eq!(size, 12);
+        assert!(&buf[size..].is_empty());
+        assert_eq!(
+            values,
+            alloc::vec![
+                EmptyOrFullCOctetString::<6>::from_static_slice(b"Hello\0").unwrap(),
+                EmptyOrFullCOctetString::<6>::from_static_slice(b"World\0").unwrap(),
+            ]
+        );
+
+        let buf = b"Hello\0World\0Hi";
+
+        // This will try to decode 11 bytes b"Hello\0World"
+        let error = Vec::<COctetString<1, 6>>::decode(buf, 11).unwrap_err();
+
+        assert!(matches!(
+            error,
+            VecDecodeError::ItemDecodeError(COctetStringDecodeError::NotNullTerminated)
+        ));
+
+        // Remaining bytes
+        let buf = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let (values, size) = Vec::<u8>::decode(buf, 5).unwrap();
+
+        assert_eq!(size, 5);
+        assert_eq!(&buf[size..], &[5, 6, 7, 8, 9]);
+        assert_eq!(values, alloc::vec![0, 1, 2, 3, 4]);
+
+        let buf = &[0, 0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9];
+
+        let (values, size) = Vec::<u16>::decode(buf, 10).unwrap();
+
+        assert_eq!(size, 10);
+        assert_eq!(&buf[size..], &[0, 5, 0, 6, 0, 7, 0, 8, 0, 9]);
+        assert_eq!(values, alloc::vec![0, 1, 2, 3, 4]);
+    }
+}
